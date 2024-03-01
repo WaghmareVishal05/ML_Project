4723dd5bc7e8d66a98e61edbe063b7b307046a35
WaghmareVishal05 <vishalwaghmare0005@gmail.com>
2024-03-01 23:50:04 +0530

    project structure logging and exception

---
+++ src/exception.py
@@ -0,0 +1,32 @@
+import sys  #provides various functions and variables that are used to manipulate different parts of the Python runtime environment
+import logging 
+
+# this give the information about on which file exception has occured and
+# which line number exception has occured this all detail stored in this variable  _,_,exc_tb
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error))
+    return error_message
+    
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+
+    def __str__(self):
+        return self.error_message
+    
+
+if __name__=="__main__":
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divided by Zero")
+        raise CustomException(e,sys)
+    
+    
